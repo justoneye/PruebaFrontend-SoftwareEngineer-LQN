79ce1a200d85784b3c91748706939f5a92ef7f31
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-25 11:37:13 -0500

    Ejercicio 1A: BuzzBazz creado

---
+++ Parte 1 - Ejercicios de lógica/BuzzBazz.js
@@ -0,0 +1,18 @@
+/**
+ * BuzzBazz.js
+ * 
+ * Imprimir los números del 0 al 100, si es par imprimir la palabra buzz en caso de que el número sea par. Sí el número es múltiplo de 5 debe imprimirse en la misma línea la palabra bazz.
+ */
+
+let x;
+
+const buzzbazz = numero => {
+	if (numero % 2 == 0) return numero +" "+ "buzz";
+    if (numero % 5 == 0) return numero +" "+ "bazz";
+    else return numero
+}
+
+for (x = 0; x <= 100; x++) 
+{
+    console.log(buzzbazz(x));   
+}
