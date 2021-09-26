6afc17f6ef6efeb217fcd3f4b4c82e19bad82136
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-25 19:08:13 -0500

    Footer creado

---
+++ Parte 2 - Website Frontend/swapi-website-page/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">           
+        <h5>SWAPI Website (C) 2021</h5>
+        <h6>SWAPI Website desarrollada por <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a></h6>       
+      </div>
+    );
+  }
+}
+
+export default Footer;
