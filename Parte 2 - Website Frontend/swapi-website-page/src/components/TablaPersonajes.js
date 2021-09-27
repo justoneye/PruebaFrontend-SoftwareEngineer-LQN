90314a283d6f1d9d5a7c39df911708ddb6d851c6
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-26 20:16:51 -0500

    Componente para ver la lista de personajes creado

---
+++ Parte 2 - Website Frontend/swapi-website-page/src/components/TablaPersonajes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TablaPersonajes extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SWAPI</h1>
+        <p>Bienvenido al Universo Star Wars, da click en la ficha del personaje que desees consultar.</p>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TablaPersonajes;
