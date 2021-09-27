99443b9608adc4933006fa8b0b9892d575dc7060
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-27 10:32:07 -0500

    Diseño de Footer cambiado a MUI

--- Parte 2 - Website Frontend/swapi-website-page/src/components/Footer.js
+++ Parte 2 - Website Frontend/swapi-website-page/src/components/Footer.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer">           
-        <h5>SWAPI Website (C) 2021</h5>
-        <h6>SWAPI Website desarrollada por <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a></h6>       
-      </div>
-    );
-  }
+import { Box, CssBaseline, BottomNavigation, Paper, Typography } from '@material-ui/core';
+
+export default function FixedBottomNavigation() {
+   return (
+    <Box sx={{ pb: 0 }}>
+      <CssBaseline />
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation>
+          <Typography variant="h5" color="inherit" component="div">
+            SWAPI Website © 2021
+          </Typography>
+          <Typography variant="h7" color="inherit" component="div">
+            SWAPI Website desarrollada por <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a>
+          </Typography>
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
 }
-
-export default Footer;
