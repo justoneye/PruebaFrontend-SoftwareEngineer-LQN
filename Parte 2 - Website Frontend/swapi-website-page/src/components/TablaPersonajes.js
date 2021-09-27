7613a8091c6022f8c5fa27879b9fcc88a8f89099
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-26 20:24:56 -0500

    Selectores agregados (BEM)

--- Parte 2 - Website Frontend/swapi-website-page/src/components/TablaPersonajes.js
+++ Parte 2 - Website Frontend/swapi-website-page/src/components/TablaPersonajes.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class TablaPersonajes extends Component {
   render() {
     return (
-      <div>
-        <h1>SWAPI</h1>
+      <div className="Personajes">
+        <h1 className="Personajes__Titulo">SWAPI</h1>
         <p>Bienvenido al Universo Star Wars, da click en la ficha del personaje que desees consultar.</p>
-        <div>
+        <div className="Personajes__Lista">
         </div>
       </div>
     );
