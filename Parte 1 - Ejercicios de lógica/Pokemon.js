bfd99e68ee50908a12f138f533f367bba0db3734
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-27 22:03:18 -0500

    Exercice 1B: Pokemon creado

---
+++ Parte 1 - Ejercicios de lógica/Pokemon.js
@@ -0,0 +1,7 @@
+/**
+ * Pokemon.js
+ * 
+ * Take the selection of 70 English Pokemon names and generate the/a sequence with the highest possible number of Pokemon names where the subsequent name starts with the final letter of the preceding name. 
+ */
+
+const pokelist = ["audino bagon baltoy banette bidoof braviary bronzor carracosta charmeleon cresselia croagunk darmanitan deino emboar emolga exeggcute gabite girafarig gulpin haxorus heatmor heatran ivysaur jellicent jumpluff kangaskhan kricketune landorus ledyba loudred lumineon lunatone machamp magnezone mamoswine nosepass petilil pidgeotto pikachu pinsir poliwrath poochyena porygon2 porygonz registeel relicanth remoraid rufflet sableye scolipede scrafty seaking sealeo silcoon simisear snivy snorlax spoink starly tirtouga trapinch treecko tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask"];  
