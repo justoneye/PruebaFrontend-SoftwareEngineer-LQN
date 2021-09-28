7ab173c81d7ceb0d2302eb1fbe5ddd18358db4b4
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-27 21:12:08 -0500

    Ejercicio 1A: Condición para múltiplos de 10 creada

--- Parte 1 - Ejercicios de lógica/BuzzBazz.js
+++ Parte 1 - Ejercicios de lógica/BuzzBazz.js
@@ -7,12 +7,14 @@
 let x;
 
 const buzzbazz = numero => {
+    if (numero % 2 == 0 && numero % 5 == 0) return numero +" "+ "buzz"+" "+ "bazz";
 	if (numero % 2 == 0) return numero +" "+ "buzz";
     if (numero % 5 == 0) return numero +" "+ "bazz";
+    
     else return numero
 }
 
-for (x = 0; x <= 100; x++) 
+for (x = 1; x <= 100; x++) 
 {
     console.log(buzzbazz(x));   
 }
