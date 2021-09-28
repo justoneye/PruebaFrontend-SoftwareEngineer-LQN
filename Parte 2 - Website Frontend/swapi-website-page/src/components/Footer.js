c29e4552282844f8203fefef7394872429c5f58d
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2021-09-28 07:49:14 -0500

    Diseño del componente Footer modificado

--- Parte 2 - Website Frontend/swapi-website-page/src/components/Footer.js
+++ Parte 2 - Website Frontend/swapi-website-page/src/components/Footer.js
@@ -8,11 +8,11 @@ export default function FixedBottomNavigation() {
       <CssBaseline />
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
         <BottomNavigation>
-          <Typography variant="h5" color="inherit" component="div">
-            SWAPI Website © 2021
-          </Typography>
-          <Typography variant="h7" color="inherit" component="div">
-            SWAPI Website desarrollada por <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a>
+          <Typography variant="h7" color="inherit" align="center" component="div">
+          <br/>
+          SWAPI Website © 2021 
+          <br/>
+          Desarrollada por <a href="https://www.linkedin.com/in/pilarcamargo/">Pilar Camargo Márquez</a>
           </Typography>
         </BottomNavigation>
       </Paper>
